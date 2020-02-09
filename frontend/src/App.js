56d605fff1c71bdd04380460c27dbc6b035719fc
tobsirl <tobster111@gmail.com>
2020-02-09 15:12:31 +0000

    Added a description on Docker Volumes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi there!</p>
+        <p>Hi there! test</p>
         <a
           className="App-link"
           href="https://reactjs.org"
