12b5807a6d7380ee1b7a24f32bc10d4dde6e04a6
tobsirl <tobster111@gmail.com>
2020-02-18 15:05:37 +0000

    Make the connection to redis with host and port

--- complex/worker/index.js
+++ complex/worker/index.js
@@ -1,1 +1,14 @@
-require('dotenv').config()
+require('dotenv').config();
+const redis = require('redis');
+
+const { redisHost, redisPort } = process.env;
+
+const redisClient = redis.createClient({
+  host: redisHost,
+  port: redisPort,
+  retry_strategy: () => 1000
+});
+
+const sub = redisClient.duplicate();
+
+
