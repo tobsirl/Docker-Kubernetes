da0307d12ecb5ea59e81c1dc78dbe48be8d105e8
tobsirl <tobster111@gmail.com>
2020-02-18 17:34:07 +0000

    Setup the postgress database connection

--- complex/server/index.js
+++ complex/server/index.js
@@ -1,1 +1,28 @@
-require('dotenv').config()
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+// Express app setup
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Postgres Client Setup
+const { Pool } = require('pg');
+const { PG_USER, PG_HOST, PG_DATABASE, PG_PASSWORD, PG_PORT } = process.env;
+const pgClient = new Pool({
+  user: PG_USER,
+  host: PG_HOST,
+  database: PG_DATABASE,
+  password: PG_PASSWORD,
+  port: PG_PORT
+});
+
+pgClient.on('error', () => console.log(`Lost PG connection`));
+
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
